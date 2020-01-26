9f87849f301b5566413e1b57b58dbd91999e0f39
gary <gary@dobbinsonline.org>
2020-01-25 22:09:29 -0500

    some tweaks

--- wan-json.py
+++ wan-json.py
@@ -3,35 +3,13 @@
 #!/usr/bin/python3
     Copyright 2017-2020 Gary Dobbins <gary@dobbinsonline.org>
 
-    A Munin plugin that acts like several plugins (each is a uniquely-named link to this file)
-    Operates in 3 modes: gather data, report as plugin, report config as plugin
-    1) When called by its own name (i.e. by cron), it scrapes data
-       from the Arris Cablemodem's status pages and stores the relevant values as a JSON file.
-    2) When called by (args[0]) the name of a plugin it emulates it reads the JSON file
-       and reports the requested values.
-    3) If 'config' is an arg, reports that plugin's config text.
-
-    Munin calls us through symlinks whose names indicate what plugin Munin thinks it's calling
-    We discern this from args[0], which will tell us what plugin to impersonate and report as.
+    A Munin plugin that acts like several plugins, thanks to Munin's 'multigraph' mode
 
     This version is attuned to the web interface of the Arris SB6183
 
-    TODO: determine if this 'multigraph' approach works
-    TODO:
-    Restructure this to store its state at the location supplied by Munin at runtime, using the following environment variables.
-    MUNIN_PLUGSTATE: directory to be used for storing files that should be accessed by other plugins
-    MUNIN_STATEFILE: single state file to be used by a plugin that wants to track its state from the last time it was requested by the same master
-    Source: http://guide.munin-monitoring.org/en/latest/develop/plugins/advanced-topics.html#storing-the-plugin-s-state
-
-    If this is restructured to create the data file when called as a plugin (instead of by cron), then
-    there needs to be a semaphore or mutex so we don't have simultaneous instances trying to refresh an old data file.
-    Pseudocode:
-        While data file is too old or missing
-            get lock?:
-                scrape data from modem, refresh data file, close it
-            else:
-                wait until lock is released
-        Proceed to open and read data file ...
+    TODO: Try converting it to 'dirtyconfig' mode, to save even more runtime
+    check the environment variable MUNIN_CAP_DIRTYCONFIG, ensure it has a value of 1
+    if so, also emit values when responding to 'config'
 """
 
 import datetime
@@ -43,18 +21,14 @@ import textwrap
 import requests
 from bs4 import BeautifulSoup
 
-SPEEDTEST_JSON_FILE = '/tmp/speedtest.json'
+SPEEDTEST_JSON_FILE = '/var/lib/munin-node/plugin-state/munin/speedtest.json'
 MODEM_STATUS_URL = 'http://192.168.100.1/'
 MODEM_UPTIME_URL = 'http://192.168.100.1/RgSwInfo.asp'
-LATENCY_TEST_CMD = (
-    "/usr/sbin/traceroute -n --sim-queries=1 --wait=1 --queries=1 --max-hops=")
-LATENCY_TEST_HOPS = 3
-LATENCY_TEST_HOST = '8.8.4.4'
+LATENCY_GATEWAY_HOPS = 3
+LATENCY_GATEWAY_HOST = '8.8.4.4'
+LATENCY_GATEWAY_CMD = "/usr/sbin/traceroute -n --sim-queries=1 --wait=1 --queries=1 --max-hops="
+LATENCY_MEASURE_CMD = "/bin/ping -W 3 -nqc 3 "
 report = {}
-# list of all the names by which we might be called, or valid args that may be present
-plugin_nouns = ['config', 'wan-downpower', 'wan-downsnr', 'wan-uppower', 'wan-uptime', 
-                'wan-error-corr', 'wan-error-uncorr',
-                'wan-ping', 'wan-speedtest', 'wan-spread']
 
 
 def main(args):
@@ -65,10 +39,22 @@ def main(args):
         reportConfig(args)
         return 0
 
-    # This is the default logic that happens when called by no plugin-noun's name.
-    # Instead of reporting we scrape the modem, do some math, and store the JSON
+    # speedtest's JSON file is generated elsewhere, we just report it to Munin
+    # TODO: instead, run the test here if the stored report is absent or is older than <constant>
+    # else read the report file and report from it
+    print('\nmultigraph wan_speedtest')
+    if openInput(SPEEDTEST_JSON_FILE) == 0:
+        downloadspeed = float(report['download'] / 1000000)
+        uploadspeed = float(report['upload'] / 1000000)
+        # recompute the miles so the lines on the graph don't coincide so much
+        distance = math.log(max(1, float(report['server']['d']) - 3)) + 10
+        print('down.value', downloadspeed)
+        print('up.value', uploadspeed)
+        print('distance.value', distance)
 
-    reportDateTime()  # get current time into the dictionary
+    # scrape the modem, do some math, and report it all
+
+    reportDateTime()  # get current time into the dictionary (obsolete)
     if not getUptimeIntoReport():  # also a handy check to see if the modem is responding
         return 1
 
@@ -76,42 +62,36 @@ def main(args):
         return 1
     getNextHopLatency()  # measured by ping
 
-    # everything past here needs report[] filled-in
-
-    print('multigraph wan_ping')
+    print('\nmultigraph wan_ping')
     print('latency.value', report['next_hop_latency'])
 
-    print('multigraph wan_downpower')
+    print('\nmultigraph wan_downpower')
     for chan in report['downpower']:
         print('down-power-ch' + chan + '.value', report['downpower'][chan])
-    # print('down-power-spread.value', report['downpowerspread'])
 
-    print('multigraph wan_downsnr')
+    print('\nmultigraph wan_downsnr')
     for chan in report['downsnr']:
         print('down-snr-ch' + chan + '.value', report['downsnr'][chan])
-    # print('down-snr-spread.value', report['downsnrspread'])
 
-    print('multigraph wan_uppower')
+    print('\nmultigraph wan_uppower')
     for chan in report['uppower']:
         print('up-power-ch' + chan + '.value', report['uppower'][chan])
-    # print('up-power-spread.value', report['uppowerspread'])
 
-    print('multigraph wan_spread')
+    print('\nmultigraph wan_spread')
     print('downpowerspread.value', report['downpowerspread'])
     print('downsnrspread.value', report['downsnrspread'])
     print('uppowerspread.value', report['uppowerspread'])
 
-    print('multigraph wan_error_corr')
+    print('\nmultigraph wan_error_corr')
     for chan in report['corrected_total']:
         print('corrected-total-ch' + chan + '.value', report['corrected_total'][chan])
 
-    print('multigraph wan_error_uncorr')
+    print('\nmultigraph wan_error_uncorr')
     for chan in report['uncorrectable_total']:
         print('uncorrected-total-ch' + chan + '.value', report['uncorrectable_total'][chan])
 
-    print('multigraph wan_uptime')
-    # report as days, so divide seconds ...
-    print('uptime.value', float(report['uptime_seconds']) / 86400.0)
+    print('\nmultigraph wan_uptime')
+    print('uptime.value', float(report['uptime_seconds']) / 86400.0)  # report as days, so divide seconds
 
     return 0
 
@@ -208,62 +188,83 @@ def getUptimeIntoReport():
 
 
 def reportConfig(args):
-    if not getStatusIntoReport():
+    print( "\n" +
+        textwrap.dedent("""\
+    multigraph wan_speedtest
+    graph_category x-wan
+    graph_title [1] WAN Speedtest
+    graph_args --base 1000 --lower-limit 0 --upper-limit 35 --rigid
+    graph_vlabel Megabits/Second
+    graph_scale no
+    distance.label V-Distance
+    distance.type GAUGE
+    distance.draw LINE
+    distance.colour aaaaaa
+    down.label Download
+    down.type GAUGE
+    down.draw LINE
+    down.colour 0066cc
+    up.label Upload
+    up.type GAUGE
+    up.draw LINE
+    up.colour 44aa99
+    graph_info Graph of Internet Connection Speed"""))
+    #  --slope-mode
+    # return 0
+
+    if not getStatusIntoReport():  # needed so we report the proper number of channels
         return 1
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_ping
     graph_title [2] WAN Latency
     graph_vlabel millliSeconds
     graph_category x-wan
     graph_args --alt-autoscale --upper-limit 100 --lower-limit 0 --rigid --allow-shrink
-    latency.label Latency to target
-    latency.colour cc2900
-    """))
+    latency.colour cc2900"""))
+    getGateway()
+    print("latency.label Latency to " + report['gateway'])
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_downpower
     graph_title [3] WAN Downstream Power
     graph_category x-wan
     graph_vlabel dB
-    graph_args --alt-autoscale --lower-limit 0 --rigid
-    """))
+    graph_args --alt-autoscale --lower-limit 0 --rigid"""))
     for chan in report['downpower']:
         print('down-power-ch' + chan + '.label', 'ch' + chan)
     # down-power-spread.label Spread
     # graph_args --alt-autoscale-max --upper-limit 10 --lower-limit 0 --rigid
     # graph_scale no
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_downsnr
     graph_title [4] WAN Downstream SNR
     graph_category x-wan
     graph_vlabel dB
-    graph_args --alt-autoscale --lower-limit 38 --rigid
-    """))
+    graph_args --alt-autoscale --lower-limit 38 --rigid"""))
     for chan in report['downsnr']:
         print('down-snr-ch' + chan + '.label', 'ch' + chan)
     # down-snr-spread.label Spread(+30)
     # graph_args --alt-autoscale  --upper-limit 50 --lower-limit 30 --rigid
     # graph_scale no
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_uppower
     graph_title [5] WAN Upstream Power
     graph_category x-wan
     graph_vlabel dB
-    graph_args --alt-autoscale
-    """))
+    graph_args --alt-autoscale"""))
     for chan in report['uppower']:
         print('up-power-ch' + chan + '.label', 'ch' + chan)
     # up-power-spread.label Spread(+38)
     # graph_args --alt-autoscale --upper-limit 50 --lower-limit 30 --rigid
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_spread
     graph_title [6] Signal Quality Spread
@@ -273,19 +274,17 @@ def reportConfig(args):
     graph_category x-wan
     downpowerspread.label Downstream Power spread
     downsnrspread.label Downstream SNR spread
-    uppowerspread.label Upstream Power spread
-    """))
+    uppowerspread.label Upstream Power spread"""))
     #  --lower-limit 0 --rigid
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_error_corr
     graph_title [7] WAN Downstream Corrected
     graph_period minute
     graph_vlabel Blocks per Minute
     graph_scale no
-    graph_category x-wan
-    """))
+    graph_category x-wan"""))
     for chan in report['corrected_total']:
         print('corrected-total-ch' + chan + '.label', 'ch' + chan)
         print('corrected-total-ch' + chan + '.type', 'DERIVE')
@@ -293,15 +292,14 @@ def reportConfig(args):
     # graph_args --alt-autoscale  --upper-limit 50 --lower-limit 30 --rigid
     # graph_args --alt-autoscale
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_error_uncorr
     graph_title [8] WAN Downstream Uncorrectable
     graph_period minute
     graph_vlabel Blocks per Minute
     graph_scale no
-    graph_category x-wan
-    """))
+    graph_category x-wan"""))
     for chan in report['uncorrectable_total']:
         print('uncorrected-total-ch' + chan + '.label', 'ch' + chan)
         print('uncorrected-total-ch' + chan + '.type', 'DERIVE')
@@ -309,7 +307,7 @@ def reportConfig(args):
     # graph_args --alt-autoscale  --upper-limit 50 --lower-limit 30 --rigid
     # graph_args --alt-autoscale
 
-    print(
+    print( "\n" +
         textwrap.dedent("""\
     multigraph wan_uptime
     graph_title [9] Modem Uptime
@@ -318,20 +316,20 @@ def reportConfig(args):
     graph_vlabel uptime in days
     graph_category x-wan
     uptime.label uptime
-    uptime.draw AREA
-    """))
+    uptime.draw AREA"""))
+    return 0
 
 
 def getGateway():  # returns success by setting report['gateway']
     global report
-    cmd = LATENCY_TEST_CMD \
-        + str(LATENCY_TEST_HOPS) \
+    cmd = LATENCY_GATEWAY_CMD \
+        + str(LATENCY_GATEWAY_HOPS) \
         + " " \
-        + LATENCY_TEST_HOST
+        + LATENCY_GATEWAY_HOST
     output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     result = '0'
     for line in output.split('\n'):
-        if line.startswith(' ' + str(LATENCY_TEST_HOPS) + ' '):
+        if line.startswith(' ' + str(LATENCY_GATEWAY_HOPS) + ' '):
             result = line.split(' ')
             if len(result) > 3:
                 result = result[3]
@@ -342,8 +340,7 @@ def getGateway():  # returns success by setting report['gateway']
 def getNextHopLatency():
     global report
     getGateway()
-    cmd = "/bin/ping -W 3 -nqc 3 " + report['gateway'] + " 2>/dev/null"
-    # cmd = "/bin/ping -W 3 -nqc 3 " +  LATENCY_TEST_HOST + " 2>/dev/null"
+    cmd = LATENCY_MEASURE_CMD + report['gateway'] + " 2>/dev/null"
     try:
         output = subprocess.check_output(cmd, shell=True).decode("utf-8")
     except subprocess.CalledProcessError:
@@ -385,9 +382,11 @@ def openInput(aFile):
         fhInput = open(aFile, 'r')
         report = json.load(fhInput)
         fhInput.close()
+        return 0
     except (FileNotFoundError, OSError, json.decoder.JSONDecodeError) as the_error:
         print("error reading", aFile, the_error, file=sys.stderr)
-        sys.exit(1)
+        # sys.exit(1)
+        return 1
 
 
 if __name__ == '__main__':
